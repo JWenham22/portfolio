2d43de7a993d65a8ddff4e19d1b42b2a9f9837e2
jwenham <jrwenham22@gmail.com>
2025-03-21 12:34:49 -0700

    finished navbar and mobile menu for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { LoadingScreen } from './components/LoadingScreen.jsx'
 import { Navbar } from './components/Navbar.jsx'
 import { MobileMenu } from './components/MobileMenu.jsx'
+import { Home } from './components/sections/Home.jsx'
 import "./index.css"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Home />
       </div>
     </>
   )
